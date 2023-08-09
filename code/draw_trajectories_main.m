84f7bfac9d22d4ec85772495549403e184d0b4b2
Yixuan Li <hyperdunk2019@mail.ustc.edu.cn>
2023-08-09 20:37:10 +0800

    add start frame to eliminate putting the animal

--- code/draw_trajectories_main.m
+++ code/draw_trajectories_main.m
@@ -1,31 +1,32 @@
 clc; clear; close all;
 
-% read
-folder_name = 'F:\1_learning\research\planarian\result\20230728\video';
-file_name = '20230729_1654.avi';
+%% read
+folder_name = 'D:\Public_repository\planarian\result\20230808\video';
+file_name = '20230808_1843.mp4';
 full_path = fullfile(folder_name,file_name);
 video = VideoReader(full_path);
 
-% save
-output_video_file = strrep(full_path,'.avi','_binarized_with_trajectory_temp.mp4');
+%% save
+output_video_file = strrep(full_path,'.mp4','_binarized_with_trajectory_temp.mp4');
 output_video = VideoWriter(output_video_file, 'MPEG-4');
+output_video.FrameRate = 2;
 open(output_video);
 
-% exclude background
+%% exclude background
 mask_of_background = get_mask_of_background(video);
 imshow(mask_of_background);
 
-% centroid distance threshold
+%% centroid distance threshold
 distance_threshold = 100;  % Set this according to your specific video
 
-% initialize
+%% initialize
 video.currentTime = 0;
 count_frame = 0;
 trajectories = {};  % Initialize to empty cell array
 has_successor = [];  % Initialize to empty array
 is_drawed = [];
 
-% loop to process each frame 1 by 1
+%% loop to process each frame 1 by 1
 while hasFrame(video)
     count_frame = count_frame + 1;
     
@@ -38,9 +39,9 @@ while hasFrame(video)
     %% save
     writeVideo(output_video, binary_frame_RGB);
 
-    % if video.currentTime > 100
-    %     break;
-    % end
+    if video.currentTime > 100
+        break;
+    end
 
 end
 close(output_video);
--- code/figure_to_video.m
+++ code/figure_to_video.m
@@ -1,7 +1,7 @@
 clc;clear;close all;
 
 % get all images 
-folder_name = 'F:\1_learning\research\planarian\data\20230728 test boundary\20230729_1654';
+folder_name = 'D:\Public_repository\planarian\data\20230808_1843';
 image_files = dir(fullfile(folder_name,'*.jpg'));
 image_files_names = {image_files.name}';
 
@@ -9,10 +9,19 @@ image_files_names = {image_files.name}';
 [~,index] = sort(cellfun(@(x) str2double(regexprep(x, '\D', '')), image_files_names));
 sorted_image_files = image_files_names(index);
 
+% cut
+start_frame = 47;
+end_frame = size(sorted_image_files,1);
+sorted_image_files = sorted_image_files(start_frame:end_frame);
+
 % create save video
+save_folder_path = 'D:\Public_repository\planarian\result\20230808\video';  % specify your desired output directory here
 [~,save_file_name] = fileparts(folder_name);
-output_folder = 'F:\1_learning\research\planarian\result\20230728\video';  % specify your desired output directory here
-video = VideoWriter(fullfile(output_folder, [save_file_name '.avi']));
+save_full_path = fullfile(save_folder_path,save_file_name);
+if ~isfolder(save_folder_path)
+    mkdir(save_folder_path);
+end
+video = VideoWriter(save_full_path,'MPEG-4');
 video.FrameRate = 2;
 
 % write
