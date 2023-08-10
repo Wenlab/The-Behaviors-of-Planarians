e09b9809ea23e1e35ffcbd6432b6b93a94d1a76d
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-08-10 08:08:10 +0800

    Create check_size.m

---
+++ code/check_size.m
@@ -0,0 +1,4 @@
+folder_path = 'F:\1_learning\research\planarian\result\20230728\video';
+file_name = '20230729_1654_binarized_with_trajectory_temp.mp4';
+full_path = fullfile(folder_path,file_name);
+video = VideoReader(full_path);
